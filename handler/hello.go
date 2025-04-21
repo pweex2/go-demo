685d7b08a8a209f0dae290b47f7aacd71b81c34b
wayne <wayne_p@greypanel.com>
2025-04-21 15:26:16 +0800

    test lint

--- handler/hello.go
+++ handler/hello.go
@@ -8,7 +8,5 @@ import (
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	resp := map[string]string{"message": "Hello, GitHub Actions!"}
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(resp); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	json.NewEncoder(w).Encode(resp)
 }
